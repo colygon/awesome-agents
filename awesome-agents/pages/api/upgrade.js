39f0e56a19dd5e52d539049bd916a91b34e20241
Collin Lowenburg <colin@lowenberg.org>
2025-12-22 02:17:48 -0800

    Fix async/await syntax in upgrade API handler

--- awesome-agents/pages/api/upgrade.js
+++ awesome-agents/pages/api/upgrade.js
@@ -1,6 +1,6 @@
 import db from '../../db.js';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
